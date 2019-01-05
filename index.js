e16aed7e645ff26e393e04e346be10ca82892d0e
Matt Oliver <mattoliver.mattoliver@gmail.com>
2019-01-05 11:01:22 -0600

    cleaning up

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const Table = require('cli-table');
 
 let table = new Table({
 		head: ['date', 'recipient', 'city', 'state', 'amount', 'description'], 
-		colWidths: [20, 20, 20, 20, 20, 20]
+		colWidths: [20, 20, 15, 10, 15, 20]
 })
 
 //spent:
 const options = {
   url: 'https://api.open.fec.gov/v1/schedules/schedule_b/?api_key=5yyI90SU3Xb73TVlv4wrEhQxYcCwMWCywQiGdYbJ&sort_hide_null=false&sort_nulls_last=true&sort=-disbursement_date&per_page=100&committee_id=C00498121&two_year_transaction_period=2018',
-  // url: 'https://api.open.fec.gov/v1/schedules/schedule_b/?api_key=5yyI90SU3Xb73TVlv4wrEhQxYcCwMWCywQiGdYbJ&sort_hide_null=false&sort_nulls_last=true&sort=-disbursement_date&per_page=100&committee_id=C00498121&two_year_transaction_period=2018',
   json: true 
 
 //received:
@@ -25,9 +24,8 @@ request(options)
     
     var i;
     for (var i =0; i < data.results.length; i++) {
-    	fecData.push({date: data.results[i].disbursement_date, name: data.results[i].recipient_name.toProperCase(), recipient_city: data.results[i].recipient_city.toProperCase(), recipient_state: data.results[i].recipient_state.toProperCase(), disbursement_amount: data.results[i].disbursement_amount, disbursement_description: data.results[i].disbursement_description.toProperCase()});
+    	fecData.push({date: data.results[i].disbursement_date, name: data.results[i].recipient_name.toProperCase(), recipient_city: data.results[i].recipient_city.toProperCase(), recipient_state: data.results[i].recipient_state, disbursement_amount: data.results[i].disbursement_amount, disbursement_description: data.results[i].disbursement_description.toProperCase()});
     }
-
     // 	console.log('On ' + data.results[i].disbursement_date + ' Roger Williams paid ' + data.results[i].recipient_name.toProperCase() + ' in ' + data.results[i].recipient_city.toProperCase() + ', ' + data.results[i].recipient_state.toProperCase() + ' $' + data.results[i].disbursement_amount + ' for ' + data.results[i].disbursement_description.toProperCase() + '.')
     
     process.stdout.write('Loading FEC data');
